7262c53dd23a481152e49c92394fef0d27d15eb8
Chris Hogan <chogan@logikos.com>
2011-11-23 16:05:57 -0500

    Updated PowerShell scripts

--- NugetPack.ps1
+++ NugetPack.ps1
@@ -1,11 +1,12 @@
 $srcRoot = '.\src'                     # relative to script directory
 $versionFile = 'SharedAssemblyInfo.cs' # relative to $srcRoot
 $outputPath = "$home\Dropbox\Packages"
-$scriptRoot = "$home\Dropbox\Scripts"
 
-. "$scriptRoot\New-Path.ps1" $outputPath
+Import-Module NugetUtilities
 
-$version = . "$scriptRoot\Get-Version.ps1" (Join-Path $srcRoot $versionFile -Resolve)
+New-Path $outputPath
 
-. "$scriptRoot\Pack-Project.ps1" Simple.Data.PostgreSql $srcRoot $version $outputPath
+$version = Get-Version (Join-Path $srcRoot $versionFile)
+
+Pack-Project Simple.Data.PostgreSql $srcRoot $version $outputPath
 
--- NugetPush.ps1
+++ NugetPush.ps1
@@ -1,8 +1,9 @@
 $srcRoot = '.\src'                     # relative to script directory
 $versionFile = 'SharedAssemblyInfo.cs' # relative to $srcRoot
 $outputPath = "$home\Dropbox\Packages"
-$scriptRoot = "$home\Dropbox\Scripts"
 
-$version = . "$scriptRoot\Get-Version.ps1" (Join-Path $srcRoot $versionFile -Resolve)
+Import-Module NugetUtilities
 
-. "$scriptRoot\Push-Project.ps1" Simple.Data.PostgreSql $srcRoot $version $outputPath
+$version = Get-Version (Join-Path $srcRoot $versionFile)
+
+Push-Project Simple.Data.PostgreSql $srcRoot $version $outputPath

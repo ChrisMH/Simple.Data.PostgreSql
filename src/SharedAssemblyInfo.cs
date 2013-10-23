e697a1d188990cdf5f82b95d270e36de81b2ab6a
Chris Hogan <hogancm@gmail.com>
2013-10-22 22:24:27 -0400

    Updated version

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -10,12 +10,12 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("Chris Hogan")]
 [assembly: AssemblyProduct("Simple.Data.PostgreSql")]
-[assembly: AssemblyCopyright("Copyright © Chris Hogan 2010-2011")]
+[assembly: AssemblyCopyright("Copyright © Chris Hogan 2010-2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("0.16.2.2")]
-[assembly: AssemblyFileVersion("0.16.2.2")]
-[assembly: AssemblyInformationalVersion("0.16.2.2")]
+[assembly: AssemblyVersion("0.17.0.0")]
+[assembly: AssemblyFileVersion("0.17.0.0")]
+[assembly: AssemblyInformationalVersion("0.17.0.0")]
